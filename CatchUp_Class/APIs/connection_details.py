7e3fb74e7547da8190e4917638cebe352a61fde5
Le-FireStarter <berriesrcute@gmail.com>
2022-07-10 00:49:05 +0100

    Re-arranged files and folders

---
+++ CatchUp_Class/APIs/connection_details.py
@@ -0,0 +1,6 @@
+details =  {'pguser' : 'postgres',
+            'password' : 'C0ff33se@son',
+            'host' : '127.0.0.1',
+            'port' : 5432,
+            'database' : 'postgres'
+            }
