ca70cec075809b2f7deecde634e2ebe65667b226
Le-FireStarter <berriesrcute@gmail.com>
2022-07-10 13:37:08 +0100

    Connection tested successfully. Testing saving & importing connection details

--- Personal/connection_details.py
+++ Personal/connection_details.py
@@ -1,5 +1,5 @@
 details =  {'pguser' : 'postgres',
-            'password' : 'C0ff33se@son',
+            'password' : '-@son',
             'host' : '127.0.0.1',
             'port' : 5432,
             'database' : 'postgres'
